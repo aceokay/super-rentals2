e4ccbf60a6441e655a17678c9d95095e72f4253e
Guest User <student@epicodus.com>
2015-09-21 16:41:23 -0700

    add awesome styling

--- app/routes/index.js
+++ app/routes/index.js
@@ -19,15 +19,3 @@ export default Ember.Route.extend({
     }
   }
 });
-
-  var cities = [{
-    id:1,
-    name:"Vienna",
-    attraction:"Lisa's appartment",
-    image:"http://cdn.decoist.com/wp-content/uploads/2012/05/most-stunning-penthouses-in-Australia.jpg"
-  }, {
-    id:2,
-    name:"Portland",
-    attraction:"Burnside Bridge",
-    image:"https://c2.staticflickr.com/4/3224/2663909610_9ffb33a085.jpg"
-  }];
